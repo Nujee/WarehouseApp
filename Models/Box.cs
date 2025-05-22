1e753442895d8e218f945d2fe3f402ee58a09128
N-u-j-e-e <310895@mail.ru>
2025-05-22 22:50:34 +0300

    Add: BaseWarehouseItem and Box classes
     BaseWarehouseItem is and abstract parent to Box and Pallet

---
+++ Models/Box.cs
@@ -0,0 +1,28 @@
+﻿namespace WarehouseApp.Models
+{
+    public sealed class Box : BaseWarehouseItem
+    {
+        public double Weight { get; set; }
+        public DateOnly? ProductionDate { get; set; }
+        public DateOnly? ExpirationDate { get; set; }
+
+        public DateOnly CalculatedExpirationDate
+        {
+            get
+            {
+                if (ExpirationDate.HasValue)
+                {
+                    return ExpirationDate.Value;
+                }
+
+                if (ProductionDate.HasValue)
+                {
+                    return ProductionDate.Value.AddDays(100);
+                }
+
+                throw new InvalidOperationException("Cannot calculate expiration date: " +
+                    "both ExpirationDate and ProductionDate are null.");
+            }
+        }
+    }
+}
