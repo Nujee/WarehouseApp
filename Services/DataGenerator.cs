95f078a857847a5d7b0dcdea71979363106360e2
N-u-j-e-e <310895@mail.ru>
2025-05-23 21:23:22 +0300

    Add: DataGenerator.cs
    DataGenerator creates a mock version of a real DB (could have made an outer DB generator and DataReader, but decided to leae it that way due to simplicity)

---
+++ Services/DataGenerator.cs
@@ -0,0 +1,51 @@
+﻿using WarehouseApp.Models;
+
+namespace WarehouseApp.Services
+{
+    public static class DataGenerator
+    {
+        private static Random _random = new Random();
+
+        public static List<Pallet> GeneratePallets(int count)
+        {
+            var pallets = new List<Pallet>();
+
+            for (int i = 0; i < count; i++)
+            {
+                var pallet = new Pallet
+                {
+                    Id = $"Pallet_{i}",
+                    Width = _random.Next(100, 200),
+                    Height = _random.Next(100, 200),
+                    Depth = _random.Next(100, 200),
+                };
+
+                int boxCount = _random.Next(1, 5);
+
+                for (int j = 0; j < boxCount; j++)
+                {
+                    var box = new Box
+                    {
+                        Id = $"Box_{i}_{j}",
+                        Width = _random.Next(25, 100),
+                        Height = _random.Next(25, 100),
+                        Depth = _random.Next(25,100),
+                        Weight = _random.Next(1, 20),
+
+                        ProductionDate = DateOnly.FromDateTime(DateTime.Now.AddDays(-_random.Next(100,365))),
+                        ExpirationDate = _random.NextDouble() > 0.5
+                            ? DateOnly.FromDateTime(DateTime.Now.AddDays(_random.Next(30, 180)))
+                            : null
+                    };
+
+                    if (pallet.CanAddBox(box))
+                        pallet.AddBox(box);
+                }
+
+                pallets.Add(pallet);
+            }
+
+            return pallets;
+        }
+    }
+}
