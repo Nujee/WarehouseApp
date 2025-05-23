1026be5cc9a58b535adaf26e546da5dda95985dd
N-u-j-e-e <310895@mail.ru>
2025-05-23 23:18:48 +0300

    Add: DataProcessor.cs
    has 2 methods: GroupByExpiration and GetTop3PalletsByExpiration

---
+++ Services/DataProcessor.cs
@@ -0,0 +1,44 @@
+﻿using WarehouseApp.Models;
+
+namespace WarehouseApp.Services
+{
+    public static class DataProcessor
+    {
+        public static void GroupByExpiration(List<Pallet> pallets)
+        {
+            var groupedByExpiration = pallets
+                .Where(pallet => pallet.Boxes.Count > 0)
+                .GroupBy(pallet => pallet.ExpirationDate)
+                .OrderBy(group => group.Key)
+                .ToList();
+
+            foreach (var group in groupedByExpiration)
+            {
+                Console.WriteLine($"Group with expiration date: {group.Key}");
+                var sortedPallets = group
+                    .OrderBy(pallet => pallet.Weight)
+                    .ToList();
+
+                foreach (var pallet in sortedPallets) 
+                { 
+                    Console.WriteLine($"{pallet.Id}, weight: {pallet.Weight} kg, volume: {pallet.Volume / 1_000_000} m^3");
+                }
+            }
+        }
+
+        public static void GetTop3PalletsByExpiration(List<Pallet> pallets)
+        {
+            var topPallets = pallets
+                .Where(pallet => pallet.Boxes.Count > 0)
+                .OrderByDescending(pallet => pallet.ExpirationDate)
+                .ThenBy(pallet => pallet.Volume)
+                .Take(3)
+                .ToList();
+
+            foreach (var pallet in topPallets)
+            {
+                Console.WriteLine($"{pallet.Id}, expiration date: {pallet.ExpirationDate}, volume: {pallet.Volume / 1_000_000} m^3");
+            }
+        }
+    }
+}
